07304905260564f075466902246fe5a55fd4a331
Idan <idanshtivi1998@email.com>
2026-02-07 15:19:08 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "48px",
+      fontWeight: "bold"
+    }}>
+      BotRank
+    </main>
+  );
+}
